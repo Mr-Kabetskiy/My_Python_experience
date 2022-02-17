649d72bfde54d2a626c70cae1e91ba7ddd60ac11
MACTEP-Python <57221680+MACTEP-Python@users.noreply.github.com>
2022-02-17 18:23:48 +0300

    Как считаю, классно рещил задачку по файловому вводу-выводу

--- index1.py
+++ index1.py
@@ -669,3 +669,25 @@ def verify(lst: list):
             if lst[i][j] == 1:
                 a.append(is_isolate(i, j, lst))
     return all(a)
+
+
+# %% 3.4.4 Файловый ввод/вывод
+
+means, subj1, subj2, subj3 = [], [], [], []
+with open('dataset_3363_4.txt') as file:
+    for line in file.readlines():
+        row = line.strip().split(';')
+        row = list(map(lambda x: int(x) if x.isdigit() else x, row))
+        means.append(sum(row[1:]) / len(row[1:]))
+        subj1.append(row[1])
+        subj2.append(row[2])
+        subj3.append(row[3])
+
+mean_subj1 = sum(subj1) / len(subj1)
+mean_subj2 = sum(subj2) / len(subj2)
+mean_subj3 = sum(subj3) / len(subj3)
+
+with open('res.txt', 'w') as output_file:
+    for m in means:
+        output_file.write(f'{m}\n')
+    output_file.write(f'{mean_subj1} {mean_subj2} {mean_subj3}')
--- testing2.py
+++ testing2.py
@@ -1,27 +1,20 @@
-# %% Добрый Python
+# %% 3.4.4 Файловый ввод/вывод
 
-s = ['1 0 0 0 0',
-     '0 0 1 0 0',
-     '0 0 0 0 0',
-     '0 1 0 1 0',
-     '0 0 0 0 0']
+means, subj1, subj2, subj3 = [], [], [], []
+with open('dataset_3363_4.txt') as file:
+    for line in file.readlines():
+        row = line.strip().split(';')
+        row = list(map(lambda x: int(x) if x.isdigit() else x, row))
+        means.append(sum(row[1:]) / len(row[1:]))
+        subj1.append(row[1])
+        subj2.append(row[2])
+        subj3.append(row[3])
 
-lst = []
-for row in s:
-    lst.append(list(map(int, row.split())))
-lst_in_c = lst
+mean_subj1 = sum(subj1) / len(subj1)
+mean_subj2 = sum(subj2) / len(subj2)
+mean_subj3 = sum(subj3) / len(subj3)
 
-
-def verify(lst: list):
-    def is_isolate(i, j, lst):
-        return sum([lst[i][j], lst[i][j + 1], lst[i + 1][j], lst[i + 1][j + 1]]) <= 1
-
-    a = []
-    for i in range(len(lst) - 1):
-        for j in range(len(lst) - 1):
-            if lst[i][j] == 1:
-                a.append(is_isolate(i, j, lst))
-    return all(a)
-
-
-verify(lst_in_c)
+with open('res.txt', 'w') as output_file:
+    for m in means:
+        output_file.write(f'{m}\n')
+    output_file.write(f'{mean_subj1} {mean_subj2} {mean_subj3}')
